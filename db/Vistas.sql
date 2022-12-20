132593706c7da3b3229f968dc62ced60778d1f43
AngelDevOs <ing.Angel_mendoza@outlook.com>
2022-12-19 21:08:46 -0600

    practicas 60%

--- db/Vistas.sql
+++ db/Vistas.sql
@@ -2,6 +2,6 @@ use andic;
 
 CREATE VIEW getInstAct AS SELECT * FROM institucion WHERE estado = 1;
 
-CREATE VIEW getpracticasact AS SELECT pr.folio_p,p.nombre, p.app, p.apm, i.nombre_ins, s.service, pr.estado, pr.proy FROM practicas AS pr INNER JOIN persona AS p ON p.id_p = pr.persona INNER JOIN servicios AS s ON s.registro_c = pr.institucion INNER JOIN institucion AS i ON i.clave = s.inst WHERE pr.estado <5;
+CREATE VIEW getpracticasact AS SELECT pr.folio_p, pr.tipo ,p.nombre, p.app, p.apm, i.nombre_ins, s.service, pr.estado, pr.proy FROM practicas AS pr INNER JOIN persona AS p ON p.id_p = pr.persona INNER JOIN servicios AS s ON s.registro_c = pr.institucion INNER JOIN institucion AS i ON i.clave = s.inst WHERE pr.estado <5;
 
 Select  * from getInstAct;
--- templates/services.php
+++ templates/services.php
@@ -17,24 +17,26 @@ $request = $model->getPracticas();
         <div class="head-col">
             <h1>Solicitud</h1>
         </div>
-        <hr class="separador"> 
+        <hr class="separador">
         <div class="body-col">
             <?php while ($item = $request->fetch_assoc()) {
                 if ($item['estado'] == 1) { ?>
                     <div class="card" id="card-<?php echo $item['folio_p'] ?>">
                         <div class="card-header">
                             <strong>
-                                <?php echo $item['nombre'].' '.$item['app'].' '.$item['apm']; ?>
+                                <?php echo $item['nombre'] . ' ' . $item['app'] . ' ' . $item['apm']; ?>
                             </strong>
                         </div>
                         <div class="card-body">
-                            <h6 class="card-title">[<?php echo strtoupper($item['nombre_ins']); ?>]</h6>
-                            <h6><?php echo $item['service']; ?></h6>
+                            <p class="name_inst"><?php echo strtoupper($item['nombre_ins']); ?></p>
+                            <h6 class="card-title">[<?php if ($item['tipo'] == 'S') echo 'SERVICIO SOCIAL';
+                                                    else echo 'RESIDENCIA PROFECIONAL' ?>]</h6>
+                            <p><?php echo $item['service']; ?></p>
                             <p class="card-text">
                                 <?php echo $item['proy']; ?>
                             </p>
                             <center>
-                                <button class="btn btn-info btn-small">
+                                <button class="btn btn-info btn-small" data-bs-toggle="modal" data-bs-target="#practiqueModal">
                                     <i class="fa fa-eye" aria-hidden="true"></i>
                                 </button>
                                 <button class="btn btn-success btn-small">
@@ -103,6 +105,64 @@ $request = $model->getPracticas();
                         </button>
                     </center>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal View -->
+<div class="modal fade" id="practiqueModal" tabindex="-1" aria-labelledby="practiqueModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="practiqueModalLabel">Detalles de Estancia</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="input-flex mb-3">
+                        <div class="input-group">
+                            <span class="input-group-text">
+                                Persona:
+                            </span>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-text">
+                                Escuela:
+                            </span>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">
+                            Carrera:
+                        </span>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="input-flex mb-3">
+                        <div class="input-group">
+                            <span class="input-group-text">
+                                Servicio:
+                            </span>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-text">
+
+                            </span>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
